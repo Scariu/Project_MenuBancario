fe3ed5839155ee7a1e68e2cf4b4e10518f967d66
Flopy <f.hidalgoalarcon@gmail.com>
2023-07-17 22:16:01 -0400

    Se agrega función a buttonListo y a todos los radioButton.

--- app/src/main/java/com/example/project_menubancario/MainActivity.kt
+++ app/src/main/java/com/example/project_menubancario/MainActivity.kt
@@ -2,13 +2,39 @@ package com.example.project_menubancario
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.example.project_menubancario.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+    private var saldo = 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        binding.buttonListo.setOnClickListener {
+            when(binding.radioGroup.checkedRadioButtonId){
+                R.id.radioButtonVerSaldo ->
+                    binding.editTextMonto.setText(saldo.toString())
+                R.id.radioButtonIngresar -> ingresarSaldo()
+                R.id.radioButtonRetirar -> retirarSaldo()
+                R.id.radioButtonSalir -> finish()
+            }
+        }
+    }
+
+    private fun retirarSaldo() {
+        saldo -= binding.editTextMonto.text.toString().toInt()
+        mostrarMensaje("Su saldo ha sido retirado correctamente.")
+    }
+
+    private fun ingresarSaldo() {
+        saldo += binding.editTextMonto.text.toString().toInt()
+        mostrarMensaje("Su saldo ha sido ingresado correctamente.")
+    }
+
+    private fun mostrarMensaje(s: String) {
+        Toast.makeText(applicationContext, s, Toast.LENGTH_LONG).show()
     }
 }
+
