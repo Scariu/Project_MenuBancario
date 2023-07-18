6af344e178bb14fb7b892d84dfe5852ab157c2ee
Flopy <f.hidalgoalarcon@gmail.com>
2023-07-17 22:39:54 -0400

    Se modifica implementación de radioButtonSaldo y se modifica diseño.

--- app/src/main/java/com/example/project_menubancario/MainActivity.kt
+++ app/src/main/java/com/example/project_menubancario/MainActivity.kt
@@ -8,21 +8,25 @@ import com.example.project_menubancario.databinding.ActivityMainBinding
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     private var saldo = 0
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         binding.buttonListo.setOnClickListener {
             when(binding.radioGroup.checkedRadioButtonId){
-                R.id.radioButtonVerSaldo ->
-                    binding.editTextMonto.setText(saldo.toString())
+                R.id.radioButtonVerSaldo -> verSaldo()
                 R.id.radioButtonIngresar -> ingresarSaldo()
                 R.id.radioButtonRetirar -> retirarSaldo()
                 R.id.radioButtonSalir -> finish()
             }
         }
     }
+    private fun verSaldo() {
+        Toast.makeText(applicationContext,"Su saldo es $saldo", Toast.LENGTH_LONG).show()
 
+    }
     private fun retirarSaldo() {
         val monto = binding.editTextMonto.text.toString().toInt()
         if(monto <= saldo){
